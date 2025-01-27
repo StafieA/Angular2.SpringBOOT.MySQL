a94b453efcba8c4413eeff85b13b4ad83b488e31
Andrei Stafie <andreistafie@gmail.com>
2025-01-27 17:39:28 +0200

    Initialize backend and frontend directories

---
+++ BackendShoppingApp/src/main/java/Stafie/BackendShoppingApp/BackendShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package Stafie.BackendShoppingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendShoppingAppApplication.class, args);
+	}
+
+}
---
+++ BackendShoppingApp/src/test/java/Stafie/BackendShoppingApp/BackendShoppingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Stafie.BackendShoppingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendShoppingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
