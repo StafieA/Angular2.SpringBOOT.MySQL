454913ef56d3877b76103cc42a67960d628efea5
Andrei Stafie <andreistafie@gmail.com>
2025-01-27 18:02:18 +0200

    Develop JPA entities

---
+++ BackendShoppingApp/src/main/java/entities/ProductCategory.java
@@ -0,0 +1,30 @@
+package entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name="shopping_category")
+// @Data -- known bug
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @JsonProperty("id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+
+}
