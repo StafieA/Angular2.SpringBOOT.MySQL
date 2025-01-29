24c6741a69fbf722886bc65c4ff4466c86da83a9
Andrei Stafie <andreistafie@gmail.com>
2025-01-29 22:21:04 +0200

    Create SearchComponent and send data to search route

---
+++ AngularShoppingApp/src/app/components/search/search.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css',
+})
+export class SearchComponent implements OnInit {
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.searchIt('pula');
+  }
+
+  searchIt(value: string) {
+    console.log('Input is ' + value);
+    this.router.navigateByUrl(`/search/${value}`);
+  }
+}
