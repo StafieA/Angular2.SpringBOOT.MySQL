2013f4f88ec8004b3669bbee6b539bca9498deb8
Andrei Stafie <andreistafie@gmail.com>
2025-02-12 22:02:57 +0200

    Work on productDetails components

--- AngularShoppingApp/src/app/components/product-details/product-details.component.ts
+++ AngularShoppingApp/src/app/components/product-details/product-details.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
   imports: [],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product!: Product;
 
+  constructor(
+    private productsService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    });
+  }
+  handleProductDetails() {
+    const productId: number = +(this.route.snapshot.paramMap.get('id') ?? 1);
+
+    this.productsService.getProduct(productId).subscribe((response) => {
+      this.product = response;
+    });
+  }
 }
