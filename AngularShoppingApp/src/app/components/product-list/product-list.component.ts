9dc82c91f494d1a647bbd529ce9d9c0f44118551
Andrei Stafie <andreistafie@gmail.com>
2025-02-13 13:48:16 +0200

    Update ProductList for pagination

--- AngularShoppingApp/src/app/components/product-list/product-list.component.ts
+++ AngularShoppingApp/src/app/components/product-list/product-list.component.ts
@@ -11,7 +11,12 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductListComponent implements OnInit {
   productList: Product[] = [];
   currentCategoryId?: number;
+  previousCategoryId?: number;
   searchMode?: boolean;
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  totalElements: number = 0;
+
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute
@@ -39,16 +44,39 @@ export class ProductListComponent implements OnInit {
   handleListProducts() {
     this.currentCategoryId = +(this.route.snapshot.paramMap.get('id') ?? 1);
 
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      (data) => {
-        this.productList = data;
-        console.log(
-          'Data received: ' + JSON.stringify(this.productList[0].imageUrl)
-        );
-      },
-      (error) => {
-        console.error('Error fetching product list:', error);
-      }
-    );
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    this.productService
+      .getProductListPaginate(
+        this.thePageNumber - 1,
+        this.thePageSize,
+        this.currentCategoryId
+      )
+      .subscribe(this.processResult());
+
+    // this.productService.getProductList(this.currentCategoryId).subscribe(
+    //   (data) => {
+    //     this.productList = data;
+    //     console.log(
+    //       'Data received: ' + JSON.stringify(this.productList[0].imageUrl)
+    //     );
+    //   },
+    //   (error) => {
+    //     console.error('Error fetching product list:', error);
+    //   }
+    // );
+  }
+
+  processResult() {
+    return (data) => {
+      this.productList = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.totalElements = data.page.totalElements;
+    };
   }
 }
