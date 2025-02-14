b149b56208a313232bf4ef7dc6304e98d23044e1
Andrei Stafie <andreistafie@gmail.com>
2025-02-14 13:29:12 +0200

    Retrieve data from cart service

--- AngularShoppingApp/src/app/components/cart-details/cart-details.component.ts
+++ AngularShoppingApp/src/app/components/cart-details/cart-details.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from '../../common/cart-item';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
   styleUrl: './cart-details.component.css',
 })
-export class CartDetailsComponent {}
+export class CartDetailsComponent implements OnInit {
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartServ: CartService) {}
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+  listCartDetails() {
+    this.cartItems = this.cartServ.cartItems;
+    this.cartServ.totalPrice.subscribe((data) => {
+      this.totalPrice = data;
+    });
+
+    this.cartServ.totalQuantity.subscribe((data) => {
+      this.totalQuantity = data;
+    });
+    this.cartServ.computeCartTotals();
+  }
+}
