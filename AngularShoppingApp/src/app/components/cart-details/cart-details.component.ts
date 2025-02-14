5d010a136943d8c35d735f68927c937cfac8e99d
Andrei Stafie <andreistafie@gmail.com>
2025-02-14 14:19:23 +0200

    Finish the HTML template

--- AngularShoppingApp/src/app/components/cart-details/cart-details.component.ts
+++ AngularShoppingApp/src/app/components/cart-details/cart-details.component.ts
@@ -1,31 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CartItem } from '../../common/cart-item';
 import { CartService } from '../../services/cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
   styleUrl: './cart-details.component.css',
 })
-export class CartDetailsComponent implements OnInit {
+export class CartDetailsComponent implements OnInit, OnDestroy {
   cartItems: CartItem[] = [];
   totalPrice: number = 0;
   totalQuantity: number = 0;
+
+  private subscriptions: Subscription[] = [];
 
   constructor(private cartServ: CartService) {}
 
   ngOnInit(): void {
     this.listCartDetails();
   }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subs) => subs.unsubscribe());
+  }
   listCartDetails() {
     this.cartItems = this.cartServ.cartItems;
-    this.cartServ.totalPrice.subscribe((data) => {
-      this.totalPrice = data;
-    });
+    this.subscriptions.push(
+      this.cartServ.totalPrice.subscribe((data) => {
+        this.totalPrice = data;
+      })
+    );
 
-    this.cartServ.totalQuantity.subscribe((data) => {
-      this.totalQuantity = data;
-    });
+    this.subscriptions.push(
+      this.cartServ.totalQuantity.subscribe((data) => {
+        this.totalQuantity = data;
+      })
+    );
     this.cartServ.computeCartTotals();
   }
 }
